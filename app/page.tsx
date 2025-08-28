9a863f57ec5dd3cdea2f71004157074b9c1beccc
Ingo Wolf <me@ingowolf.au>
2025-08-28 19:50:18 +1000

    add minimal page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="flex w-full h-screen justify-center items-center text-center p-2">
+      <Link
+        href="https://github.com/Inglan/nextjs-app-template"
+        target="_blank"
+      >
+        <h1 className="text-4xl">nextjs-app-template</h1>
+      </Link>
+    </div>
+  );
 }
